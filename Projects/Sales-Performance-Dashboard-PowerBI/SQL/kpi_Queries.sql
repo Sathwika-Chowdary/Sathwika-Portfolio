ef0313c795cb5e9373706d6aef5e5fee5c92d398
Sathwika-Chowdary <157745801+Sathwika-Chowdary@users.noreply.github.com>
2026-02-19 17:34:49 -0800

    Update kpi_Queries.sql

--- Projects/Sales-Performance-Dashboard-PowerBI/SQL/kpi_Queries.sql
+++ Projects/Sales-Performance-Dashboard-PowerBI/SQL/kpi_Queries.sql
@@ -1,1 +1,25 @@
+-- KPI validation using SQL (after applying the same cleaning rules)
 
+-- Total Revenue
+SELECT SUM(Quantity * UnitPrice) AS TotalRevenue
+FROM online_retail
+WHERE InvoiceNo NOT LIKE 'C%'
+  AND Quantity > 0
+  AND UnitPrice > 0
+  AND CustomerID IS NOT NULL;
+
+-- Total Orders
+SELECT COUNT(DISTINCT InvoiceNo) AS TotalOrders
+FROM online_retail
+WHERE InvoiceNo NOT LIKE 'C%'
+  AND Quantity > 0
+  AND UnitPrice > 0
+  AND CustomerID IS NOT NULL;
+
+-- Total Customers
+SELECT COUNT(DISTINCT CustomerID) AS TotalCustomers
+FROM online_retail
+WHERE InvoiceNo NOT LIKE 'C%'
+  AND Quantity > 0
+  AND UnitPrice > 0
+  AND CustomerID IS NOT NULL;
